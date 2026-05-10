7fcef311a6e844d444b118b8f2b09a65e52ab566
PedroEsmeraldo <pedroesmeraldooffice@gmail.com>
2026-05-10 17:03:36 -0400

    removidos

--- atividade5/config.sh
+++
@@ -1,5 +0,0 @@
-#Essa linha é muito importante por não ter nenhum conteúdo nela, por isso, ela não faz nada
-
-#já essa linha tem um comando invisivel que também não faz nada
-
-#MELANCIAAAAAA Sushi Sushi Sushi Sushi Strogonoff Sushi Sushi Sushi
--- atividade7/codigo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-> relatorio.txt
-
-find /home -perm 777 >> relatorio.txt
-find / -perm -4000 >> relatorio.txt
-awk -F: '$3 == 0 {print $1}' /etc/passwd >> relatorio.txt
-
